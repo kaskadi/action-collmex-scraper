4fd4fdfb8ff3220b90cdbbb29b3771f6f1483656
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-03 13:54:58 +0200

    Added update repository helper

--- helpers/update-repo.js
+++ helpers/update-repo.js
@@ -1,4 +1,30 @@
+const fs = require('fs')
+const deepEqual = require('fast-deep-equal')
+
 module.exports = (satzarten) => {
-  const fs = require('fs')
-  fs.writeFileSync('checks/satzarten.json', JSON.stringify(satzarten, null, 2), 'utf-8')
+  const data = JSON.stringify(satzarten, null, 2)
+  const root = process.env.GITHUB_ACTIONS ? '/home/runner/work/_actions/kaskadi/action-collmex-scrapper/master/' : `${process.cwd()}/`
+  if (!process.env.GITHUB_ACTIONS || process.env.GITHUB_REPOSITORY === 'kaskadi/action-collmex-scrapper') {
+    // when we're testing the action, we just want to write the result into a data folder in the test folder
+    fs.writeFileSync(`${root}test/data/satzarten.json`, data, 'utf-8')
+  } else {
+    const filePath = process.env.SATZARTEN_PATH
+    const currentData = fs.readFileSync(filePath, 'utf-8').trim()
+    if (!deepEqual(satzarten, JSON.parse(currentData))) {
+      updateFiles(filePath, currentData, data)
+      pushChanges(root)
+    } else {
+      console.log('INFO: data identical to current one, not proceeding to update data file...')
+    }
+  }
+}
+
+function updateFiles (filePath, currentData, newData) {
+  const backupPath = `${filePath.slice(0, -5)}.backup${filePath.slice(-5)}`
+  fs.writeFileSync(backupPath, currentData, 'utf-8')
+  fs.writeFileSync(filePath, newData, 'utf-8')
+}
+
+function pushChanges (root) {
+  console.log(root)
 }
