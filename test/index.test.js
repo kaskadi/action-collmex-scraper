dbae999c839e053fa4b73bfe0339a8f8811af849
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-03 13:54:39 +0200

    Updated tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,31 +1,25 @@
 /* eslint-env mocha */
-// const childProc = require('child_process')
+const childProc = require('child_process')
 const chai = require('chai')
 chai.should()
 
-// *************
-// Write your tests here
 describe('action-collmex-scrapper', function () {
-  // this.timeout(10000)
-
-  // uncomment this if you want to raise the timeout cap for your whole test (when you need to perform long async tasks). You can also follow the same logic per describe block or per it block to increase the timeout cap
-  // if your describe/it callbacks are arrow functions, you need to use the following syntax: describe('...', () => {}).timeout(...) but be aware that this won't apply to hooks!
+  this.timeout(10000)
+  it('should fetch data from Collmex docs', async function () {
+    await execMain()
+  })
 })
-// *************
 
-// awaitable function to execute the main script of this action
-// function execMain () {
-//   return new Promise((resolve, reject) => {
-//     childProc.exec('node index', (err, stdout, stderr) => {
-//       if (err === null) {
-//         console.log(stdout)
-//         resolve(true)
-//       } else {
-//         console.log(stderr)
-//         resolve(false)
-//       }
-//     })
-//   })
-// }
-
-// just add any helpers you may need
+function execMain () {
+  return new Promise((resolve, reject) => {
+    childProc.exec('node index', (err, stdout, stderr) => {
+      if (err === null) {
+        console.log(stdout)
+        resolve(true)
+      } else {
+        console.log(stderr)
+        resolve(false)
+      }
+    })
+  })
+}
