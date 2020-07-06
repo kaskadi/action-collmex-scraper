bc21aef9693e1755e940834677e768c26cd68d7d
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-06 13:11:55 +0200

    Extended scraping range to cover whole docs

--- helpers/scraper/get-endpoint-hrefs.js
+++ helpers/scraper/get-endpoint-hrefs.js
@@ -1,5 +1,12 @@
 module.exports = (page) => {
-  return page.$$eval('#dv-hilfe p', ps => ps.map(p => p.textContent.trim()))
-    .then(ps => ps.indexOf('Abfragen'))
-    .then(matchingIndex => page.$$eval(`#dv-hilfe p:nth-of-type(${matchingIndex + 1}) + ul a`, as => as.map(a => a.href)))
+  return page.evaluate(extractEndpointHrefs)
+  // return page.$$eval('#dv-hilfe p', ps => ps.map(p => p.textContent.trim()))
+  //   .then(ps => ps.filter(p => p !== 'Szenarien'))
+  //   .then(matchingIndex => page.$$eval(`#dv-hilfe p:nth-of-type(${matchingIndex + 1}) + ul a`, as => as.map(a => a.href)))
+}
+
+function extractEndpointHrefs () {
+  const excludedSections = ['Inhalt', 'Szenarien']
+  const sections = Array.from(document.querySelectorAll('#dv-hilfe p')).filter(section => !excludedSections.includes(section.textContent.trim()))
+  return sections.flatMap(section => Array.from(section.nextSibling.querySelectorAll('a')).map(a => a.href))
 }
