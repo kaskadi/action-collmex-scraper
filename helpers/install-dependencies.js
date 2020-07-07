d9c253f01dedb7c81f38e9d6eab8a1d57551421e
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-07 11:36:01 +0200

    Hotfix: added logging to mark NPM install output

--- helpers/install-dependencies.js
+++ helpers/install-dependencies.js
@@ -7,7 +7,9 @@ module.exports = () => {
   process.chdir(__dirname)
   process.chdir('..')
   console.log('INFO: installing action dependencies...')
+  console.log('************ NPM ouput ************')
   spawnSync('npm', ['i', '--only=prod'], { stdio: 'inherit' })
+  console.log('************ End of NPM ouput ************')
   console.log('SUCCESS: dependencies installed!')
   process.chdir(callingRepo)
 }
