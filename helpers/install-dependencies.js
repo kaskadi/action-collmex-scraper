86f779505eab8e0560c16b2cc258e2ab6f1bdeed
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-07 10:48:51 +0200

    Test: testing different approach for action root

--- helpers/install-dependencies.js
+++ helpers/install-dependencies.js
@@ -1,12 +1,10 @@
 module.exports = () => {
-  console.log('install dependencies helper')
   if (!process.env.GITHUB_ACTIONS || process.env.GITHUB_REPOSITORY === 'kaskadi/action-collmex-scraper') {
-    console.log('leaving')
     return
   }
-  console.log('installing')
   const { spawnSync } = require('child_process')
   const callingRepo = process.cwd()
+  console.log(__dirname)
   const actionRepo = '/home/runner/work/_actions/kaskadi/action-collmex-scraper/master/'
   process.chdir(actionRepo)
   console.log('INFO: installing action dependencies...')
