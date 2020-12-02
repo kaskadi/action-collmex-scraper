887129485b8475a2b89668087b2af7dda5d91dc3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-12-02 11:51:26 +0100

    Added grouping for logs when in GitHub Actions

--- src/main.js
+++ src/main.js
@@ -1,21 +1,23 @@
+const startPupeteer = require('./helpers/start-pupeteer.js')
+const scraper = require('./helpers/scraper/scraper.js')
+const updateRepo = require('./helpers/update-repo/update-repo.js')
+const { group } = require('@actions/core')
+
 async function main () {
-  const startPupeteer = require('./helpers/start-pupeteer.js')
-  const scraper = require('./helpers/scraper/scraper.js')
-  const updateRepo = require('./helpers/update-repo/update-repo.js')
-  const { browser, page } = await startPupeteer()
-    .catch(err => {
-      console.log(err)
-      process.exit()
-    })
-  const satzarten = await scraper(page, browser)
-    .catch(err => {
-      console.log(err)
-      process.exit()
-    })
-    .finally(async () => {
-      await browser.close()
-    })
-  updateRepo(satzarten)
+  const { browser, page } = await startPupeteer().catch(handleError)
+  const satzarten = await group('Scrapping Collmex API documentation for CSV schema of every endpoints',
+    () => scraper(page, browser)
+      .catch(handleError)
+      .finally(() => browser.close())
+  )
+  await group(`Updating ${process.env.GITHUB_REPOSITORY} with synced Collmex API data`,
+    async () => updateRepo(satzarten)
+  )
+}
+
+function handleError (err) {
+  console.log(err)
+  process.exit()
 }
 
 main()
